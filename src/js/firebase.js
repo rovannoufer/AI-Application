cb67e1b6fbc5f67c2fd14ecacffcca3612b81451
rovannoufer <noufermohamed007@gmail.com>
2024-07-07 15:57:05 +0530

    firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzjYHGVPRy46-ro6dxUzGiRevBeZPYKi4",
+  authDomain: "ai-application-6b8f8.firebaseapp.com",
+  projectId: "ai-application-6b8f8",
+  storageBucket: "ai-application-6b8f8.appspot.com",
+  messagingSenderId: "997283704636",
+  appId: "1:997283704636:web:eacceeed162e354e04f839",
+  measurementId: "G-BQ461G41VM"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
